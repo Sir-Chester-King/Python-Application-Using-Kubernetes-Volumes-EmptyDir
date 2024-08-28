fab4cfaf6f997b2a929ba942104c68919d6b2a4e
Sir-Chester-King <177522077+Sir-Chester-King@users.noreply.github.com>
2024-08-28 15:40:41 +0200

    Updated code.

--- Store_Data/Store_data.py
+++ Store_Data/Store_data.py
@@ -4,26 +4,12 @@ import os
 
 
 def Store_Data_Into_Volume(user):
-    # This is the PATH inside the Project Directory (current directory)
-    # -> Python-Application-Using-Kubernetes-Volumes/Store_Data/Store_data.py
-    absolutepath = os.path.abspath(__file__)
+    # This is the PATH inside the Docker Container Volume
+    path_volume_docker = "/Docker_Directory/Storage/User_Data.txt"
 
-    # Go up one level -> Python-Application-Using-Kubernetes-Volumes/Store_Data
-    one_level_up = os.path.dirname(absolutepath)
-
-    # Go up two levels -> Python-Application-Using-Kubernetes-Volumes
-    two_level_up = os.path.dirname(one_level_up)
-
-    # Check if the directory inside the project exist or not.
+    # Check if the directory inside the volume exist or not.
     # In case it doesn't exist, it is created.
-    directory_storage = os.path.join(two_level_up, "Storage")
-
-    # Name of the file will contain the user's data.
-    file_name = "Data_Users.txt"
-
-    # Path of the txt file where the user's data will stored
-    # -> Python-Application-Using-Kubernetes-Volumes/Storage/Data_Users.txt
-    file_path = os.path.join(directory_storage, file_name)
+    directory_storage = os.path.dirname(path_volume_docker)
 
     # Check if the "Storage" directory exist, in case it does not exit, it created.
     if not os.path.exists(directory_storage):
@@ -32,13 +18,13 @@ def Store_Data_Into_Volume(user):
 
     # Writing the data in the file.
     try:
-        with open(file_path, 'a+') as storage_file:
+        with open(path_volume_docker, 'a+') as storage_file:
             storage_file.write(f"Name: {user.get_name()}\n")
             storage_file.write(f"Surname: {user.get_surname()}\n")
             storage_file.write(f"Address: {user.get_address()}\n")
             storage_file.write(f"Phone Number: {user.get_phone_number()}\n\n")
     except FileNotFoundError:
-        with open(file_path, 'w+') as storage_file:
+        with open(path_volume_docker, 'w+') as storage_file:
             storage_file.write(f"Name: {user.get_name()}\n")
             storage_file.write(f"Surname: {user.get_surname()}\n")
             storage_file.write(f"Address: {user.get_address()}\n")
--- View_Users/View_users.py
+++ View_Users/View_users.py
@@ -5,33 +5,22 @@ import Main_Code.main
 
 def list_users_volume():
     Main_Code.main.clean_console()
+    my_var = os.getenv("PYTHONPATH")
 
-    # This is the PATH inside the Project Directory (current directory)
-    # -> Python_App_Using_Kubernetes/Store_Data/Store_data.py
-    absolutepath = os.path.abspath(__file__)
+    print(f"MY_VARIABLE is: {my_var}")
 
-    # Go up one level -> Python_App_Using_Kubernetes/Store_Data
-    one_level_up = os.path.dirname(absolutepath)
+    # This is the PATH inside the Docker Container Volume
+    path_volume_docker = "/Docker_Directory/Storage/User_Data.txt"
 
-    # Go up two levels -> Python_App_Using_Kubernetes
-    two_level_up = os.path.dirname(one_level_up)
+    directory_storage = os.path.dirname(path_volume_docker)
 
-    # Check if the directory inside the project exist or not.
-    # In case it doesn't exist, it is created.
-    directory_storage = os.path.join(two_level_up, "Storage")
-
-    # Name of the file will contain the user's data.
-    file_name = "Data_Users.txt"
-
-    # Path of the txt file where the user's data will stored
-    file_path = os.path.join(directory_storage, file_name)
-
+    # Check if the directory inside the volume exist or not.
     if not os.path.exists(directory_storage):
         print(f"The directory {directory_storage} was not found")
 
     # Try statesman to read all the file "USER_DATA" into the Docker volume
     try:
-        with open(file_path, 'r') as storage_file:
+        with open(path_volume_docker, 'r') as storage_file:
             content = storage_file.read()
             print("List Users:", end="\n")
             print(content)
